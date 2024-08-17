1198cc229ca9d098710a04f5d54719c375f994a2
stayzer <stayzer2@gmail.com>
2024-08-17 20:43:39 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -33,27 +33,48 @@ function accordion() {
 }
 accordion();
 
-function bindModal(trigger, modal, close) {
-  (trigger = document.querySelector(trigger)),
-    (modal = document.querySelector(modal)),
-    (close = document.querySelector(close));
-
-  const body = document.body;
-
-  trigger.addEventListener("click", (e) => {
-    e.preventDefault();
-    modal.style.display = "flex";
-    body.classList.add("locked");
-  });
-  close.addEventListener("click", () => {
+const modalClose = document.querySelector(".modal__close");
+const modal = document.querySelector(".modal__wrapper");
+const body = document.body;
+window.addEventListener("beforeunload", function (e) {
+  e.preventDefault();
+  e.returnValue = "";
+  openPopupOnExit(); // здесь вызывается функция для открытия попапа при попытке покинуть страницу
+});
+modalClose.addEventListener("click", () => {
+  modal.style.display = "none";
+  body.classList.remove("locked");
+});
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
     modal.style.display = "none";
     body.classList.remove("locked");
-  });
-  modal.addEventListener("click", (e) => {
-    if (e.target === modal) {
-      modal.style.display = "none";
-      body.classList.remove("locked");
-    }
-  });
+  }
+});
+function openPopupOnExit() {
+  modal.style.display = "flex";
+  body.classList.add("locked");
 }
-bindModal(".modal__btn", ".modal__wrapper", ".modal__close");
+setTimeout(function () {
+  modal.style.display = "flex";
+  body.classList.add("locked");
+}, 30000);
+// function bindModal(trigger, modal, close) {
+//   (trigger = document.querySelector(trigger)),
+//     (modal = document.querySelector(modal)),
+//     (close = document.querySelector(close));
+
+//   const body = document.body;
+
+//   close.addEventListener("click", () => {
+//     modal.style.display = "none";
+//     body.classList.remove("locked");
+//   });
+//   modal.addEventListener("click", (e) => {
+//     if (e.target === modal) {
+//       modal.style.display = "none";
+//       body.classList.remove("locked");
+//     }
+//   });
+// }
+// bindModal(".modal__btn", ".modal__wrapper", ".modal__close");
